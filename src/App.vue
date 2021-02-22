5cff2b2a29f7f0a4de4ec145add97c0fc3ee7e85
Daniel Larionov <danielarionov@gmail.com>
2021-02-22 18:13:09 +0200

    added map option

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" :class="typeof weather.main!='undefined' && weather.main.temp>16 ?
     'warm':''">
     <main> 
+      <h1>You may search a location or scroll the map to get the weather</h1>
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="search..." 
         v-model="query"
@@ -14,22 +15,60 @@
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
-          <div class="weather">{{weather.weather[0].main}}</div>
+          <div class="weather">{{weather.weather[0].main}} </div>
         </div>
+        <br><br>
+      </div>
+      <div class="mapcomp">
+          <l-map
+          v-if="showMap"
+          :zoom="zoom"
+          :center="center"
+          :options="mapOptions"
+          style="height: 80%"
+          @update:center="centerUpdate"
+          @update:zoom="zoomUpdate"
+        >
+          <l-tile-layer
+            :url="url"
+            :attribution="attribution"
+          />
+        </l-map>
       </div>
     </main>
   </div>
 </template>
 
 <script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer } from "vue2-leaflet";
 export default {
   name: 'App',
+  components: {
+    LMap,
+    LTileLayer,
+  },
   data(){
     return{
+      //weather data
       api_key: '4243d879dd010783de0d2852a8b9ab5d',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query:'',
-      weather:{}
+      weather:{},
+      //map data
+      zoom: 13,
+      center: latLng(32.81079451181896, 34.982786178588874),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      currentZoom: 11.5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+      
     }
   },
  methods:{
@@ -37,13 +76,32 @@ export default {
      if(e.key=="Enter"){
        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
        .then(res=>{
-         return res.json();
+         if(res.ok){
+          return res.json();
+         }
+         else{
+           alert("Input is invalid");
+           return res.json();
+         }
        }).then(this.setResult)
      }
    },
+    fetchWeatherMap(){
+       fetch(`${this.url_base}weather?lat=${this.currentCenter.lat}&lon=${this.currentCenter.lng}&units=metric&APPID=${this.api_key}`)
+       .then(res=>{
+         if(res.ok){
+          return res.json();
+         }
+         else{
+           alert("Input is invalid");
+           return res.json();
+         }
+       }).then(this.setResult)
+   },
    setResult(results){
      this.weather=results;
-   },
+     this.center=latLng(results.coord.lat, results.coord.lon);
+    },
    dateBuilder(){
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -53,8 +111,22 @@ export default {
       let month = months[d.getMonth()];
       let year = d.getFullYear();
       return `${day} ${date} ${month} ${year}`;
-   }
+   },
+       zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+      this.fetchWeatherMap();
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
  }
+
 }
 </script>
 
@@ -141,5 +213,19 @@ main{
 }
 #app.warm {
   background-image: url(./assets/warm-bg.jpg);
+}
+.mapcomp{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  height: 500px;
+  width: 100%;
+}
+h1{
+  text-align: center;
+  padding: 1%;
+  font-family: 'montserrat',sans-serif;
+  color: #FFF;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
-
+import 'leaflet/dist/leaflet.css';
 new Vue({
   render: h => h(App),
 }).$mount('#app')
